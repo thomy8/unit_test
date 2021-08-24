578c5b11344d192dd8a63b4b8115dde78dc35a1f
thomy8 <75118049+thomy8@users.noreply.github.com>
2021-08-24 18:22:10 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+npm install
+
+
+Untuk menjalankan program
+
+npm run dev
+
+Untuk menjalankan Testing
+
+npm run test
+
---
+++ dummy/students.js
@@ -0,0 +1,18 @@
+const students = [
+    {
+      id: 1,
+      name: 'Thomas',
+      age: 24,
+    },
+    {
+      id: 2,
+      name: 'Fajar',
+      age: 26,
+    },
+    {
+      id: 3,
+      name: 'Nugraha',
+      age: 19,
+    },
+ ];
+ export default students;
